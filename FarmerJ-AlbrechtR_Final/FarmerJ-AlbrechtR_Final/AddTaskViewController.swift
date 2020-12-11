8fde481f4f853084f43e95457c1cc3ca0d0abb70
farmerjordan <61027632+farmerjordan@users.noreply.github.com>
2020-12-11 12:55:04 -0800

    Added 2 View Controller Files
    Made a TableViewController and an AddTaskViewController file

---
+++ FarmerJ-AlbrechtR_Final/FarmerJ-AlbrechtR_Final/AddTaskViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AddTaskViewController.swift
+//  FarmerJ-AlbrechtR_Final
+//
+//  Created by Jordan Farmer on 12/11/20.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Do any additional setup after loading the view
+    }
+}
---
+++ FarmerJ-AlbrechtR_Final/FarmerJ-AlbrechtR_Final/TableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TableViewController.swift
+//  FarmerJ-AlbrechtR_Final
+//
+//  Created by Jordan Farmer on 12/11/20.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
