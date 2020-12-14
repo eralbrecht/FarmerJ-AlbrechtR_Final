36f5bbd32dc0578a9e1a51c8f5c5bd89bf6f3f96
farmerjordan <61027632+farmerjordan@users.noreply.github.com>
2020-12-13 22:28:02 -0800

    Keyboard Shifts Screen Up Not Working
    Works for text fields, does not work for the notes section which is textview. I modified the code so I thought it would work but it might not be something as easy as changing out the variables, might need new implementation.

--- FarmerJ-AlbrechtR_Final/FarmerJ-AlbrechtR_Final/AddTaskViewController.swift
+++ FarmerJ-AlbrechtR_Final/FarmerJ-AlbrechtR_Final/AddTaskViewController.swift
@@ -16,6 +16,8 @@ class AddTaskViewController: UIViewController {
     
     var activeTextField: UITextField? = nil
     
+    var activeTextView: UITextView? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //Do any additional setup after loading the view
@@ -33,6 +35,9 @@ class AddTaskViewController: UIViewController {
         let tapRecognizer = UITapGestureRecognizer()
         tapRecognizer.addTarget(self, action: #selector(AddTaskViewController.didTapView))
         self.view.addGestureRecognizer(tapRecognizer)
+        
+        TitleText.delegate = self
+        NotesText.delegate = self
     }
     
     @objc func keyboardWillShow(notification: NSNotification) {
@@ -46,7 +51,22 @@ class AddTaskViewController: UIViewController {
           var shouldMoveViewUp = false
 
           // if active text field is not nil
-          if let activeTextField = activeTextField {
+
+    
+        if let activeTextView = activeTextView {
+            
+            let bottomOfTextView = activeTextView.convert(activeTextView.bounds, to: self.view).maxX
+    
+            let topOfKeyboard = self.view.frame.height - keyboardSize.height
+            
+            // if the bottom of textView is below the top of keyboard, move up
+            
+            if bottomOfTextView > topOfKeyboard {
+                shouldMoveViewUp = true
+            }
+        }
+        
+        if let activeTextField = activeTextField {
 
             let bottomOfTextField = activeTextField.convert(activeTextField.bounds, to: self.view).maxY;
             
@@ -83,4 +103,17 @@ extension AddTaskViewController : UITextFieldDelegate {
   func textFieldDidEndEditing(_ textField: UITextField) {
     self.activeTextField = nil
   }
+}
+
+extension AddTaskViewController: UITextViewDelegate {
+    // when user selects a textview, this method will be called
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        //set the activeTextView to the selected textView
+        self.activeTextView = textView
+    }
+    
+    //when user clicks 'done' or dismisses the keyboard
+    func textViewDidEndEditing(_ textView: UITextView) {
+        self.activeTextView = nil
+    }
 }
